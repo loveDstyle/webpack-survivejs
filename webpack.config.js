bb55f9310bb5cf7d39144c58db16e7ed90264248
wuhao <wuhao@hualala.com>
2018-07-09 14:21:35 +0800

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Webpack demo",
+        }),
+    ],
+    devServer: {
+        // Display only errors to reduce the amount of output.
+        stats: "errors-only",
+
+        // Parse host and port from env to allow customization.
+        //
+        // If you use Docker, Vagrant or Cloud9, set
+        // host: options.host || "0.0.0.0";
+        //
+        // 0.0.0.0 is available to all network devices
+        // unlike default `localhost`.
+        host: process.env.HOST, // Defaults to `localhost`
+        port: process.env.PORT, // Defaults to 8080
+        open: true, // Open the page in browser
+    },
+
+};
