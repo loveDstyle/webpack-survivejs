9cc74346feddf913950fc413d12270c4e65d4ca4
loveDstyle <abc55289511@gmail.com>
2018-07-15 21:37:49 +0800

    add css and styles

---
+++ config/webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const merge = require("webpack-merge");
+const parts = require("./webpack.part");
+
+const path = require("path");
+const glob = require("glob");
+
+const PATHS = {
+    app: path.join(__dirname, "src"),
+};
+
+const common = merge([
+    {
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: "Webpack demo",
+            }),
+        ],
+    },
+]);
+
+const production = merge([
+    parts.extractCSS({
+        use: "css-loader",
+    }),
+    parts.purifyCSS({
+        paths: glob.sync(`${PATHS.app}/**/*.js`, { nodir: true }),
+    }),
+]);
+
+const development = merge([
+    parts.devServer({
+        // Customize host/port here if needed
+        host: process.env.HOST,
+        port: process.env.PORT,
+    }),
+    parts.loadCSS(),
+]);
+
+module.exports = ({test: mode}) => {
+    console.log(`It's ${mode} mode`);
+    if (mode === "production") {
+        return merge(common, production, { mode });
+    }
+
+    return merge(common, development, { mode });
+};
